53f289a859a59b8eed35e18d6b53033d2b27914b
MUJEEB UR REHMAN <muhammadmujeeburrehman613@gmail.com>
2024-06-09 01:11:06 +0500

    Add project files.

---
+++ SOAP_CRUD/Employee.asmx.cs
@@ -0,0 +1,85 @@
+﻿using SOAP_CRUD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SOAP_CRUD
+{
+    /// <summary>
+    /// Summary description for Employee
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Employee : System.Web.Services.WebService
+    {
+        EmployeesEntities db = new EmployeesEntities();
+
+        [WebMethod]
+        public Model.Employee GetEmployeeDetails(int id)
+        {
+
+
+            Model.Employee emp = new Model.Employee();
+            emp = db.Employees.Where(x => x.Id ==
+            id).FirstOrDefault(); return emp;
+        }
+        [WebMethod]
+        public List<Model.Employee> GetEmployeeList()
+        {
+            List<Model.Employee> emp = new List<Model.Employee>(); emp = db.Employees.ToList(); return emp;
+        }
+        [WebMethod]
+        public string AddEmployee(string name, int age, string address, int salary)
+        {
+            Model.Employee employee = new Model.Employee
+            {
+                Name = name,
+                Age = age,
+                Salary = salary,
+                Address = address
+            }; try
+            {
+                db.Employees.Add(employee); db.SaveChanges(); return "Employee Added Successfully";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        [WebMethod]
+        public string UpdateEmployee(int id, string name, int age, string address, int salary)
+        {
+            Model.Employee employee = new Model.Employee { Id = id, Name = name, Age = age, Salary = salary, Address = address }; try
+            {
+                Model.Employee emp = new Model.Employee(); emp = db.Employees.Where(x => x.Id == employee.Id).FirstOrDefault(); emp.Name = employee.Name; emp.Age = employee.Age; emp.Address = employee.Address; emp.Salary = employee.Salary; db.SaveChanges(); return "Employee Updated Successfully";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        [WebMethod]
+        public string DeleteEmployee(int id)
+        {
+            try
+            {
+                Model.Employee emp = new Model.Employee(); emp = db.Employees.Where(x => x.Id == id).FirstOrDefault(); db.Employees.Remove(emp); db.SaveChanges(); return "Employee Deleted Successfully";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ SOAP_CRUD/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SOAP_CRUD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SOAP_CRUD")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef321858-db50-4b54-be8a-ea2b5ab4882a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
